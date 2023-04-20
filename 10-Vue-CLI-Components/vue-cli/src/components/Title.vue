c06792d7a9b0b4684f2415e3eb2604923a3d19be
Abdurrahman Aydın <chatlaks_1992@hotmail.com>
2023-04-20 04:53:35 +0300

    Vue Componenet  İşlemi

---
+++ 10-Vue-CLI-Components/vue-cli/src/components/Title.vue
@@ -0,0 +1,21 @@
+<template>
+  <h3>{{ title }}</h3>
+</template>
+
+<script>
+export default {
+data() {
+    return {
+        title: 'Vue 3 Components Kullanımı',
+    }
+},
+}
+</script>
+
+<style>
+h3{
+  color: red;
+  font-size: 14px;
+  text-align: center;
+}
+</style>
