a4124f7ef41789a62ca0b57c74b71206d7c42187
Abdurrahman Aydın <chatlaks_1992@hotmail.com>
2023-04-21 00:14:00 +0300

    Vue Comp Arası Event Aktarımı

---
+++ 12-Vue-CLI-Comp-Event-Aktarımı/vue-cli/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+<Title :titleText="baslik"/>
+<Button @clean="cleanInput"/>
+<input type="text" ref="inputText">
+
+</div>
+
+
+</template>
+
+<script>
+import Title from "./components/Title.vue"
+import Button from "./components/Button.vue"
+
+export default {
+  name: 'App',
+  
+  components:{
+    Title,
+    Button
+  },
+  data(){
+    return{
+      baslik:"Vue3 Başlık"
+    }
+  },
+  methods:{
+    cleanInput(){
+      this.$refs.inputText.value = ""
+    }
+  }
+ 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 12-Vue-CLI-Comp-Event-Aktarımı/vue-cli/src/components/Title.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>{{ titleText }}</h3>
+  </div>
+</template>
+
+<script>
+
+export default {
+   props:["titleText"],
+    data(){
+        return {
+          
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
