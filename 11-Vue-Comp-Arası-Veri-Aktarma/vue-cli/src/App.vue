ef774417e4e211b73de8104ac5d6c2dbb973fe3f
Abdurrahman Aydın <chatlaks_1992@hotmail.com>
2023-04-20 23:54:48 +0300

    Vue Comp Arası Veri Aktarımları

---
+++ 11-Vue-Comp-Arası-Veri-Aktarma/vue-cli/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<Title :titleText="baslik"/>
+
+</template>
+
+<script>
+import Title from "./components/Title.vue"
+
+export default {
+  name: 'App',
+  
+  components:{
+    Title
+  },
+  data(){
+    return{
+      baslik:"Vue3 Başlık"
+    }
+  }
+ 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
