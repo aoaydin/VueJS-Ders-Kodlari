1315d185c5d851923f02dbecc13bad43e2ca85eb
Abdurrahman Aydın <chatlaks_1992@hotmail.com>
2023-04-27 22:54:37 +0300

    Update AddProject

--- 15-Vue-Uygulama-2-Project-Tracker/project-tracker/src/views/AddProject.vue
+++ 15-Vue-Uygulama-2-Project-Tracker/project-tracker/src/views/AddProject.vue
@@ -4,7 +4,7 @@
       <label>Project Title: </label>
       <input type="text" v-model="title" required>
       <label>Project Details: </label>
-      <textarea v-model="details" required></textarea>
+      <textarea v-model="description" required></textarea>
       <button>Add Project</button>
     </form>
   </div>
@@ -15,54 +15,69 @@ export default {
   data() {
     return {
       title: '',
-      details: ''
+      description: ''
     }
   },
   methods: {
     handleSubmit() {
-      console.log(this.title, this.details);
+      let project = {
+        title: this.title,
+        description: this.description,
+        complete: true
+      }
+      fetch('http://localhost:3000/projects', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(project)
+      })
     }
   }
 }
 </script>
 
 <style>
-form{
+form {
   padding: 20px;
   border-radius: 5px;
 
 }
-label{
+
+label {
   display: block;
-  color:#bbb;
+  color: #bbb;
   text-transform: uppercase;
-  font-size:14px;
+  font-size: 14px;
   font-weight: bold;
   letter-spacing: 1px;
-  margin:20px 0 10px 0;
+  margin: 20px 0 10px 0;
 }
-input{
+
+input {
   padding: 10px;
-  border:0;
-  border-bottom: 3px solid #ee0808 ;
+  border: 0;
+  border-bottom: 3px solid #ee0808;
   width: 50%;
   box-sizing: border-box;
 }
-input:focus{
-  border-bottom: 5px solid #73ff00 ;
+
+input:focus {
+  border-bottom: 5px solid #73ff00;
 }
-textarea{
-  border-bottom: 3px solid #ee0808 ;
+
+textarea {
+  border-bottom: 3px solid #ee0808;
   padding: 10px;
   width: 50%;
   box-sizing: border-box;
   height: 100px;
 }
-textarea:focus{
 
-  border-bottom: 5px solid #73ff00 ;
+textarea:focus {
+
+  border-bottom: 5px solid #73ff00;
 }
-form button{
+
+form button {
   display: block;
   margin: 20px auto 0;
   background: #ee0808;
@@ -71,10 +86,10 @@ form button{
   font-size: 20px;
   padding: 10px;
 }
-form button:hover{
+
+form button:hover {
   background: #73ff00;
 }
-
 </style>
 
 
